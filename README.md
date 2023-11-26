b35b3fb58e86d05eb4f9bfe202827b3013d2b640
Enes Özeren <enesozeren7@gmail.com>
2023-11-26 23:09:42 +0100

    adaboost structure is added

--- README.md
+++ README.md
@@ -2,12 +2,12 @@
 This project is about my implementation of some machine learning algorithms from scratch while I'm learning about them.
 
 Done:
-
 1) Deep Feedforward Neural Network
 2) Decision Tree
 3) Random Forest
 
 In Progress:
+1) AdaBoost
 
 Not Started:
-1) Gradient Boosted Trees
+
--- decision_trees/adaboost.py
+++ decision_trees/adaboost.py
@@ -13,9 +13,27 @@ class AdaBoostClassifier():
         Initialize the object with the hyperparameters
         n_base_learner: # of base learnes in the model (base learners are DecisionTree with max_depth=1)
         """
+        self.n_base_learner = n_base_learner
 
-    def train(self):
+    def _fit_base_learner(self, X_train, y_train, sample_weights):
         pass
+
+    def _update_sample_weights(self, base_learner, X_train, y_train, sample_weights):
+        pass
+    
+    def train(self, X_train, y_train):
+        """
+        trains base learners with given feature and label dataset 
+        """
+
+        # Initialize equal sample weights
+        m = X_train.shape[0]
+        sample_weights = np.full(shape=m, fill_value=1.0/m)
+        self.base_learner_list = []
+        for i in range(self.n_base_learner):
+            base_learner = self._fit_base_learner(X_train, y_train, sample_weights)
+            self.base_learner_list.append(base_learner)
+            sample_weights = self._update_sample_weights(base_learner, X_train, y_train, sample_weights)
 
     def predict_proba(self):
         pass
